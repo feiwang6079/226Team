baa624f8ebf9b2ae9d2d286c5b4b8589af5ffd81
Wei Su <willsuwei@hotmail.com>
2019-04-17 17:29:44 -0700

    cafe api implemented

--- Server/Cafe/src/main/java/com/cmpe226/cafe/web/CafeController.java
+++ Server/Cafe/src/main/java/com/cmpe226/cafe/web/CafeController.java
@@ -1,6 +1,8 @@
 package com.cmpe226.cafe.web;
 
 import com.cmpe226.cafe.Cafe;
+import com.cmpe226.cafe.Message;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,10 +16,18 @@ public class CafeController {
     @Autowired
     CafeRowMapper cafeRowMapper;
 
-    @GetMapping("/cafeTest")
-    public List<Cafe> customer(){
+    @GetMapping("/cafe")
+    public Message customer(){
         List<Cafe> cafes = cafeRowMapper.findAllCafe();
-        return  cafes;
-    }
 
+        String data;
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            data = mapper.writeValueAsString(cafes);
+        } catch (Exception e) {
+            data = "";
+        }
+
+        return new Message(200, "Success", data);
+    }
 }
