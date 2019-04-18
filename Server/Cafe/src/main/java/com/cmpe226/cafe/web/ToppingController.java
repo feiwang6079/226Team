8b8b35f0e099e44e319b183ed666d89674f84bb8
Wei Su <willsuwei@hotmail.com>
2019-04-17 17:34:41 -0700

    topping api

--- Server/Cafe/src/main/java/com/cmpe226/cafe/web/ToppingController.java
+++ Server/Cafe/src/main/java/com/cmpe226/cafe/web/ToppingController.java
@@ -1,7 +1,9 @@
 package com.cmpe226.cafe.web;
 
+import com.cmpe226.cafe.Message;
 import com.cmpe226.cafe.Topping;
 import com.cmpe226.cafe.ToppingRowMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +16,17 @@ public class ToppingController {
     @Autowired
     ToppingRowMapper toppingRowMapper;
     @GetMapping("/listAllToppings")
-    public List<Topping> listAllToppings(){
-        return toppingRowMapper.findAllTps();
+    public Message listAllToppings(){
+        List<Topping> result = toppingRowMapper.findAllTps();
+
+        String data;
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            data = mapper.writeValueAsString(result);
+        } catch (Exception e) {
+            data = "";
+        }
+
+        return new Message(200, "Success", data);
     }
 }
