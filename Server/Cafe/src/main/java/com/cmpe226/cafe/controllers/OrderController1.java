ac72429323ac95152a58331320cac4a197c262d3
Wei Su <willsuwei@hotmail.com>
2019-04-24 18:33:57 -0700

    create order implemented

--- Server/Cafe/src/main/java/com/cmpe226/cafe/controllers/OrderController1.java
+++ Server/Cafe/src/main/java/com/cmpe226/cafe/controllers/OrderController1.java
@@ -3,13 +3,11 @@ package com.cmpe226.cafe.controllers;
 import com.cmpe226.cafe.models.Drink;
 import com.cmpe226.cafe.models.Message;
 import com.cmpe226.cafe.models.Orders;
-import com.cmpe226.cafe.repositories.TeaService;
 import com.cmpe226.cafe.services.OrderService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,43 +15,18 @@ public class OrderController1 {
     @Autowired
     OrderService orderService;
 
-    @Autowired
-    private TeaService teaService;
 
     @PostMapping("/orders")
-    public Message createOrder(//@RequestParam long order_id,
-//                                  @RequestParam double total_price,
-                                  //@RequestParam String status,
-                                  //@RequestParam Timestamp t,
-//                                  @RequestBody long cus_id,
-//                                  @RequestBody String re_id,
-//                               @RequestBody String drinks_json
+    public Message createOrder(
                                @RequestBody Orders orders
                                ) {
 
-//        Orders orders = new Orders();
-//        orders.setCus_id(cus_id);
-//        orders.setRe_id(re_id);
-//        orders.setStatus("unpaid");
-//
-//        List<Drink> drinks = new ArrayList<>();
-//        Drink d1 = new Drink("50%", "30%",
-//                23, "emp_01", "Tieguanyin", "boba");
-//        d1.saveOrders(orders);
-//        drinks.add(d1);
-//        Drink d2 = new Drink("0%", "0%",
-//                23, "emp_01", "Tieguanyin", "boba");
-//        d2.saveOrders(orders);
-//        drinks.add(d2);
-//        orders.saveDrinks(drinks);
-//
-//
-//        double total_price = 0;
-//        for(Drink d: drinks) {
-//            total_price += teaService.findByTeaName(d.getTea_name()).getPrice();
-//        }
-//        orders.setTotal_price(total_price);
-//
+        List<Drink> drinks = orders.getDrinks();
+        for (Drink drink : drinks) {
+            drink.saveOrders(orders);
+        }
+
+        orders.setStatus("unpaid");
         orders = orderService.save(orders);
 
         ObjectMapper mapper = new ObjectMapper();
