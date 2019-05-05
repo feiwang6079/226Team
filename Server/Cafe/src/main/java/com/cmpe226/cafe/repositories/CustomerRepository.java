471254194f65ccc51169b61bd1af53c64bd79606
Wei Su <willsuwei@hotmail.com>
2019-05-05 11:55:33 -0700

    stored procedure implemented

--- Server/Cafe/src/main/java/com/cmpe226/cafe/repositories/CustomerRepository.java
+++ Server/Cafe/src/main/java/com/cmpe226/cafe/repositories/CustomerRepository.java
@@ -4,6 +4,8 @@ import com.cmpe226.cafe.models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -29,4 +31,7 @@ public interface CustomerRepository extends JpaRepository<Customer, String> {
 
     @Query(value = "select account_balance from customer where cus_id = ?1", nativeQuery = true)
     public double getAccountBalance(long cus_id);
+
+    @Procedure(name = "PAY")
+    public String payOrder(@Param("my_cus_id") Long cur_cus_id, @Param("my_order_id") Long cur_order_id);
 }
